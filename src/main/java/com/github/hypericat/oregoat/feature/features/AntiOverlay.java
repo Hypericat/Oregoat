134a3ae16813bd7a544af7c30e08f4008a31da9e
HypericatsFramework <charlesolivier.malo@gmail.com>
2025-05-16 15:49:30 -0400

    Added Anti Overlay Base

---
+++ src/main/java/com/github/hypericat/oregoat/feature/features/AntiOverlay.java
@@ -0,0 +1,44 @@
+package com.github.hypericat.oregoat.feature.features;
+
+import com.github.hypericat.oregoat.event.EventHandler;
+import com.github.hypericat.oregoat.event.events.ReceivePacketEvent;
+import com.github.hypericat.oregoat.event.events.SendPacketEvent;
+import com.github.hypericat.oregoat.feature.Feature;
+import net.minecraft.network.Packet;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+public class AntiOverlay extends Feature implements ReceivePacketEvent, SendPacketEvent {
+
+    @Override
+    public void onReceivePacket(Packet<?> packet, CallbackInfo ci) {
+
+    }
+
+    @Override
+    public void onSendPacket(Packet<?> packet, CallbackInfo ci) {
+
+    }
+
+
+    @Override
+    protected void onEnable() {
+        EventHandler.register(ReceivePacketEvent.class, this);
+        EventHandler.register(SendPacketEvent.class, this);
+    }
+
+    @Override
+    protected void onDisable() {
+        EventHandler.unregister(ReceivePacketEvent.class, this);
+        EventHandler.unregister(SendPacketEvent.class, this);
+    }
+
+    @Override
+    public String getName() {
+        return "Anti Overlay";
+    }
+
+    @Override
+    public String[] getAliases() {
+        return new String[] {"Overlay", "No Overlay"};
+    }
+}
